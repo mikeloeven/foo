a89223adff47a89d8693c1d0575e6a8f9507b761
mikeloeven <mikeloeven@gmail.com>
2014-08-08 12:12:25 -0400

    "Update Repo"

---
+++ spec/requests/bars_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "Bars" do
+
+  describe "home page" do
+
+    it "should have the content Hi" do
+
+      visit '/bar/home'
+
+
+      expect(page).to have_content('Hi!')
+
+
+    end
+
+  end
+
+end
